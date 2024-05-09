62b7f3808785792ab50fdfda55e46994b0679c06
JayBigGuy10 <jayden.pl@Outlook.com>
2024-05-09 12:03:39 +1200

    add python __name__ == main code

--- backend/OCRFunction.py
+++ backend/OCRFunction.py
@@ -1,3 +1,9 @@
-def OCRFunction(path):
+def OCRFunction(inputPath, outputPath):
     data = []
     return data
+
+if __name__ == "__main__":
+    inputPath = ""
+    outputPath = ""
+    print("Running OCR Function")
+    OCRFunction(inputPath,outputPath)
--- backend/STTFunction.py
+++ backend/STTFunction.py
@@ -1,3 +1,9 @@
-def OCRFunction(path):
+def STTFunction(path):
     data = []
     return data
+
+if __name__ == "__main__":
+    inputPath = ""
+    outputPath = ""
+    print("Running STT Function")
+    STTFunction(inputPath,outputPath)

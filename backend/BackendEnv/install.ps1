be1eb4bcda4e8f624e6aa016f311d89d31ee8033
Juhck <jackunsworth81@gmail.com>
2024-05-12 12:06:51 +1200

    began initial subtitle function, added whisper and ffmpeg, edited install script

--- backend/BackendEnv/install.ps1
+++ backend/BackendEnv/install.ps1
@@ -3,3 +3,6 @@ python -m venv --copies BackendEnv
 cd BackendEnv
 ./scripts/activate
 pip install -r requirements.txt
+$relativePath = Join-Path -Path $PWD -ChildPath "/ffmpeg/bin"
+$env:Path += ";$relativePath"
+$env:Path
--- backend/STTFunction.py
+++ backend/STTFunction.py
@@ -1,9 +1,14 @@
-def STTFunction(path):
-    data = []
-    return data
+import whisper
+from whisper.utils import get_writer
+
+def STTFunction(path, id):
+    model = whisper.load_model("base")
+    result = model.transcribe(path)
+    writer = get_writer("vtt", "static/video/")
+    writer(result, str(id) + ".vtt")
+    #print(f' The text in video: \n {result["text"]}')
 
 if __name__ == "__main__":
-    inputPath = ""
-    outputPath = ""
+    inputPath = """static/video/subtitles.mp4"""
     print("Running STT Function")
-    STTFunction(inputPath,outputPath)
+    STTFunction(inputPath, 2)

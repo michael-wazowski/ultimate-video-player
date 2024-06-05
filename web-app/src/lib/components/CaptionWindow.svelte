9be502aef4422a8166d91a29c851346e4bcbfaa4
JayBigGuy10 <jayden.pl@Outlook.com>
2024-06-05 12:08:14 +1200

    Add search box (broken cause of key shortcuts)

--- web-app/src/lib/components/CaptionWindow.svelte
+++ web-app/src/lib/components/CaptionWindow.svelte
@@ -28,9 +28,27 @@
     async function scrollToTarget(){
         await container?.scrollTo({top:  (await getTargetOffset(currentCueStartTime))-(container.clientHeight/2), behavior: 'smooth'}); // - containerheight / 2 to try and always center current caption
     }
+
+    let searchTerm = "";
+	// resets language menu if search input is used
+	
+	const searchCues = () => {	
+		return captions = captions.filter(cue => {
+			let cueText = cue.content.toLowerCase();
+			return cueText.includes(searchTerm.toLowerCase())
+		});
+	}
 </script>
 
 <div bind:this={container} class="hidden-scrollbar" style="width: 100%; height: 100%; overflow-y: scroll;">
+    <div id="search-input-cont">
+        <input type="text" 
+                     id="search-field" 
+                     placeholder="Enter Search Term" 
+                     autocomplete="off"
+                     bind:value={searchTerm}
+                     on:input|stopPropagation={searchCues} />
+    </div> 
 {#each captions as cue, i}
 <a href="" on:click={() => { currentTimeSeconds = cue.start}}><span id="{cue.start}" class="inactive-cue" style="{(currentTimeSeconds >= cue.start && currentTimeSeconds < cue.end) ? "color: #f4f4f4" : "text-decoration: none;"}">{cue.content} </span></a>
 {/each}
@@ -70,4 +88,20 @@
     a {
         text-decoration: none;
     }
+
+    #search-input-cont {
+		width: 40%;
+		display: flex;
+		align-items: center;
+		margin: 0 0 0 10px;
+	}
+
+	#search-field {
+		width: 100%;
+		font-size: 1.3rem;
+		border: 1px solid gray;
+		border-radius: 5px;
+		padding: 8px;
+		margin: 0 10px 0;
+	}
 </style>
